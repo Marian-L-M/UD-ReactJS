0835027fc24af5f856b65887d70c4c016f85fa90
MarianMaschkeRGST <m.maschke@rgst.net>
2022-03-02 22:59:31 +0900

    styling and pokemon details

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Pokedex from './containers/Pokedex'
+import AppNavigator from './components/AppNavigator'
+import PokemonDetails from './containers/PokemonDetails'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <AppNavigator />
+      <Route path="/" component={Pokedex} />
+      <Route path="/pokemon/:id" component={PokemonDetails} />
+    </Router>
+  )
 }
 
-export default App;
+export default App
